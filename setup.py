ee85b5197497c37f263c76d2c3b281a69faf71ea
jkortman <james.kortman@gmail.com>
2018-04-03 20:04:45 +0930

    Added structure for python package use

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mud',
+      version='0.1',
+      description='A music data parsing library for deep learning projects.',
+      url='http://github.com/jkortman/mud',
+      author='jkortman',
+      author_email='james.kortman@gmail.com',
+      license='MIT',
+      packages=['mud'],
+      zip_safe=False)
