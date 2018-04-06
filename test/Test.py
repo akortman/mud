bb3a41c7cc1435188e2de1cf789d5bcc720e6e15
jkortman <james.kortman@gmail.com>
2018-04-06 16:51:01 +0930

    Muted musicXML warnings when running test suite

--- test/Test.py
+++ test/Test.py
@@ -1,6 +1,10 @@
 import unittest
+import music21
 
 if __name__ == '__main__':
+    # mute music21 warnings.
+    music21.environment.UserSettings()['warnings'] = 0
+
     loader = unittest.TestLoader()
     start_dir = './test/'
     suite = loader.discover(start_dir)
