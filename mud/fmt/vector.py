37d24a3c17dc7e1b74fc60962b9404de148c1cca
jkortman <james.kortman@gmail.com>
2018-04-10 15:11:56 +0930

    Added make_vector validation

--- mud/fmt/vector.py
+++ mud/fmt/vector.py
@@ -4,6 +4,7 @@ Utilities for building feature vectors from notes.
 
 import feature
 import numpy as np
+from ..event import Event
 
 class VectorBuilder(object):
     def make_vector(self, obj):
@@ -21,6 +22,8 @@ class EventVectorBuilder(VectorBuilder):
         return sum([f.dim() for f in self._features])
 
     def make_vector(self, event):
+        if not isinstance(event, Event):
+            raise ValueError('EventVectorBuilder only formats Events')
         vecs = [feature.make_subvector(event) for feature in self._features]
         return np.concatenate(vecs)
 
