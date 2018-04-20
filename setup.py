52dbf105a8fbff53a01cfc40c70024e896505c90
jkortman <james.kortman@gmail.com>
2018-04-20 20:46:38 +0930

    Tweaked package description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='mud',
       version='0.1',
-      description='A music data parsing library for deep learning projects.',
+      description='A music data preperation library for machine learning projects',
       url='http://github.com/jkortman/mud',
       author='jkortman',
       author_email='james.kortman@gmail.com',
