dae19df28316788a4e75c8a5469e65181c853fd0
jkortman <james.kortman@gmail.com>
2018-04-20 20:41:39 +0930

    Added tests for BarData

--- test/test_fmt_piece_data.py
+++ test/test_fmt_piece_data.py
@@ -49,7 +49,39 @@ class TestTimeSliceData(unittest.TestCase):
         self.assertEqual(ts_data.events[1].labels, (9, 1))
 
 class TestBarData(unittest.TestCase):
-    pass
+    def test(self):
+        span = mud.Span([
+            (mud.Note('C4', 1), mud.Time(0)),
+            (mud.Note('G5', 1), mud.Time(0)),
+            (mud.Rest(      1), mud.Time(1)),
+            (mud.Note('C4', 2), mud.Time(2)),
+            (mud.Note('A4', 2), mud.Time(2)),
+        ])
+        bar_data = mud.fmt.BarData(span, formatter, slice_resolution=0.5)
+
+        self.assertEqual(len(bar_data.timeslices), 8)
+        self.assertTrue([isinstance(ts_data, mud.fmt.TimeSliceData)
+                            for ts_data in bar_data.timeslices],
+                        [True for _ in bar_data.timeslices])
+        
+        ts_data = bar_data.timeslices[1]
+        self.assertEqual(len(ts_data.events), 2)
+
+        self.assertEqual(ts_data.events[0].vec.tolist(),
+                         [1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.assertEqual(ts_data.events[0].labels, (0, 0))
+
+        self.assertEqual(ts_data.events[1].vec.tolist(),
+                         [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.assertEqual(ts_data.events[1].labels, (7, 0))
+
+        # Test rest.
+        ts_rest_data = bar_data.timeslices[2]
+        self.assertEqual(len(ts_rest_data.events), 1)
+        self.assertEqual(ts_rest_data.events[0].vec.tolist(),
+                         [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+        self.assertEqual(ts_rest_data.events[0].labels, (None, 1))
 
 class TestPieceData(unittest.TestCase):
-    pass
+    def test(self):
+        pass
