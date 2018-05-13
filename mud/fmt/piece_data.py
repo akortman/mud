48c348d55f79b7b54640cd5082fbb1fc7d197218
jkortman <james.kortman@gmail.com>
2018-05-13 16:24:26 +0930

    Added an option to discard rests in time slices if an empty list is desired for rests

--- mud/fmt/piece_data.py
+++ mud/fmt/piece_data.py
@@ -6,12 +6,16 @@ class EventData(object):
         self.labels = formatter.make_labels(event)
 
 class TimeSliceData(object):
-    def __init__(self, timeslice, formatter):
-        self.events = [EventData(e, formatter) for e in timeslice.sliced_events()]
+    def __init__(self, timeslice, formatter, discard_rests=False):
+        is_rest = (discard_rests and all(event.is_rest() for event in timeslice))
+        if is_rest:
+            self.events = []
+        else:
+            self.events = [EventData(e, formatter) for e in timeslice.sliced_events()]
 
 class BarData(object):
-    def __init__(self, bar, formatter, slice_resolution):
-        self.timeslices = [TimeSliceData(ts, formatter)
+    def __init__(self, bar, formatter, slice_resolution, discard_rests=False):
+        self.timeslices = [TimeSliceData(ts, formatter, discard_rests)
                            for ts in bar.generate_slices(slice_resolution)]
 
 class PieceData(object):
@@ -21,7 +25,7 @@ class PieceData(object):
     Designed purely for iterating over the vectors/labels during training;
     anything more complicated than that should be done to a mud.Piece.
     '''
-    def __init__(self, piece, formatter, slice_resolution):
+    def __init__(self, piece, formatter, slice_resolution, discard_rests=False):
         if isinstance(piece, self.__class__):
             raise NotImplementedError('Can\'t copy PieceData yet')
         elif not isinstance(piece, Piece):
@@ -29,6 +33,6 @@ class PieceData(object):
         
         self.bars = []
         for bar in piece.bars():
-            fmt_bar = BarData(bar, formatter, slice_resolution)
+            fmt_bar = BarData(bar, formatter, slice_resolution, discard_rests)
             self.bars.append(fmt_bar)
 
