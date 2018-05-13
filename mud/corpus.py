c4fbd2292eb7aa2bb513b0153e4da0d968938bbb
jkortman <james.kortman@gmail.com>
2018-05-13 16:25:04 +0930

    Added option for discarding rests and reporting for number of rejected pieces

--- mud/corpus.py
+++ mud/corpus.py
@@ -26,6 +26,7 @@ class Corpus(AbstractCorpus):
         patterns: an iterable of patterns (ie '*.musicxml') to load into the corpus.
         '''
         self._pieces = []
+        self._num_rejected = 0
         if from_file is not None:
             if len(patterns) > 1:
                 raise ValueError('Should not provide patterns if loading from file')
@@ -43,23 +44,30 @@ class Corpus(AbstractCorpus):
     def pieces(self):
         return self._pieces
 
+    @property
+    def num_rejected(self):
+        return self._num_rejected
+
     def load_piece(self, piece):
         self._pieces.append(Piece(piece))
 
-    def format_data(self, formatter, slice_resolution):
-        return DataCorpus(self, formatter, slice_resolution)
+    def format_data(self, formatter, slice_resolution, discard_rests=False):
+        return DataCorpus(self, formatter, slice_resolution, discard_rests)
 
     def filter(self, *filters):
         '''
         Filter out pieces that do not adhere to a set of filters.
         see: piece_filter.py
         '''
+        len_old = len(self._pieces)
         for f in filters:
             self._pieces = [p for p in self._pieces if f(p)]
+        self._num_rejected += len_old - len(self._pieces)
 
 class DataCorpus(AbstractCorpus):
-    def __init__(self, corpus, formatter, slice_resolution):
-        self._data = [PieceData(p, formatter, slice_resolution) for p in corpus.pieces]
+    def __init__(self, corpus, formatter, slice_resolution, discard_rests=False):
+        self._data = [PieceData(p, formatter, slice_resolution, discard_rests)
+                      for p in corpus.pieces]
 
     def size(self):
         return len(self._data)
@@ -67,4 +75,11 @@ class DataCorpus(AbstractCorpus):
     @property
     def data(self):
         return self._data
+
+    def __iter__(self):
+        return self._data.__iter__()
+    
+    def __len__(self):
+        return len(self._data)
+
 
