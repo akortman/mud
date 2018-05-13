804573757c3e5a75254cb493bfe4fc8647e6c8bb
jkortman <james.kortman@gmail.com>
2018-05-13 16:22:53 +0930

    Made binvec module usable to library users

--- mud/fmt/__init__.py
+++ mud/fmt/__init__.py
@@ -1,5 +1,6 @@
 from .data import EventDataBuilder
 from .piece_data import EventData, TimeSliceData, BarData, PieceData
+from .binary_vector import binvec
 
 from . import label
 from . import feature
