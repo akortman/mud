bdc70c890fd508fbbd446ac8e5a9921c997516c5
James Kortman <james.kortman@gmail.com>
2019-07-22 15:15:56 +0930

    Made Settings accessible from users of mud and added a method for editing settings

--- mud/__init__.py
+++ mud/__init__.py
@@ -4,6 +4,7 @@ from .span          import Span
 from .piece         import Piece
 from .timeslice     import SlicedEvent, TimeSlice
 from .corpus        import Corpus, DataCorpus
+from .settings      import settings
 
 from . import fmt
 from . import piece_filter
--- mud/settings.py
+++ mud/settings.py
@@ -2,16 +2,27 @@
 defaults for mud
 '''
 
+from typing import Any
+
 _defaults_dict = {
     'resolution': 1.0 / 8.0, # in quarter notes: these are 32nd notes
 }
 
 class _Settings(object):
     def __init__(self, **kwargs):
-        self._resolution = kwargs['resolution']
+        self._settings = {
+            key: kwargs[key] if key in kwargs else _defaults_dict[key]
+            for key in _defaults_dict.keys()
+        }
 
     @property
     def resolution(self):
-        return self._resolution
+        return self._settings['resolution']
+
+    def set(self, setting: str, value: Any):
+        if setting in _defaults_dict:
+            self._settings[setting] = value
+        else:
+            raise ValueError(f'Unknown setting string `{setting}`')
 
 settings = _Settings(**_defaults_dict)
